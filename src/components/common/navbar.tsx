e1944456a1ab745e5add0cd72f6036e106052a76
Devell Robinson <devell.robinson@icloud.com>
2025-11-23 06:41:18 +0000

    feat: refactor Navbar component to enable mobile using Chakra UI and improve layout

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,21 +1,52 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
+import NextLink from 'next/link';
+import { HStack, Container, Link as ChakraLink, Button, useBreakpointValue } from '@chakra-ui/react'
 
 export default function Navbar() {
+
+    const current = useBreakpointValue({
+        base: "base",
+        sm: "sm",
+        md: "md",
+        lg: "lg",
+        xl: "xl",
+        "2xl": "2xl",
+    });
+
     return (
         <nav>
-            <Link href="/">
-                <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={46} />
-            </Link>
-            <Link href="/">Home</Link>
-            <Link href="/about-us">About Us</Link>
-            <Link href="/services">Services</Link>
-            <Link href="/contact">Contact</Link>
-            <a href="tel:+16034961535">
-                <button>Call Us Today</button>
-            </a>
+            <Container h={'10vh'} zIndex={1} position={"absolute"} w={'100vw'} >
+                {current !== "base" && current !== "sm" ? (
+                    <HStack justifyContent={'space-evenly'}>
+                        <ChakraLink asChild><NextLink href="/">Home</NextLink></ChakraLink>
+                        <ChakraLink asChild><NextLink href="/about-us">About Us</NextLink></ChakraLink>
+                        <ChakraLink asChild><NextLink href="/services">Services</NextLink></ChakraLink>
+                        <ChakraLink asChild><NextLink href="/contact">Contact</NextLink></ChakraLink>
+                        <ChakraLink asChild>
+                            <Button>
+                                <NextLink href="tel:+16034961535">Call Us Today</NextLink>
+                            </Button>
+                        </ChakraLink>
+                    </HStack>
+                ) : (
+                    <HStack justifyContent={'space-between'} py={3.5} px={1}>
+                        <ChakraLink asChild >
+                            <NextLink href="/">
+                                <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={48} />
+                            </NextLink>
+                        </ChakraLink>
+                        <Button>
+                            Menu
+                        </Button>
+
+                    </HStack>
+                )}
+
+
+            </Container>
         </nav>
+
     )
 };
