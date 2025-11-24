d9c4e475066650dbd0958e5598093a9eca0f7bcd
Devell Robinson <devell.robinson@icloud.com>
2025-11-24 05:06:42 +0000

    feat: add font configuration for Geist and Poppins using next/font for improved typography

---
+++ src/components/ui/fonts.ts
@@ -0,0 +1,24 @@
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+const geistSans = Geist(
+    {
+        variable: "--font-geist-sans",
+        subsets: ["latin"],
+    }
+
+);
+const geistMono = Geist_Mono(
+    {
+        variable: "--font-geist-mono",
+        subsets: ["latin"],
+    }
+
+);
+const poppins = Poppins(
+    {
+        weight: ["800"],
+        variable: "--font-poppins",
+        subsets: ["latin"],
+    }
+
+);
+export { poppins, geistSans, geistMono };
