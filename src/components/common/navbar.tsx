62802e4a063eb2d872c4560057d9674de0b5615f
Devell Robinson <devell.robinson@icloud.com>
2025-11-24 01:47:15 +0000

    feat: enhance Navbar component layout with Chakra UI for improved navigation and styling

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -17,18 +17,41 @@ export default function Navbar() {
 
     return (
         <nav>
-            <Container h={'10vh'} zIndex={1} position={"absolute"} w={'100vw'} >
+            <Container h={'10vh'} zIndex={1} position={"absolute"} w={'100vw'} py={8}>
                 {current !== "base" && current !== "sm" ? (
-                    <HStack justifyContent={'space-evenly'}>
-                        <ChakraLink asChild><NextLink href="/">Home</NextLink></ChakraLink>
-                        <ChakraLink asChild><NextLink href="/about-us">About Us</NextLink></ChakraLink>
-                        <ChakraLink asChild><NextLink href="/services">Services</NextLink></ChakraLink>
-                        <ChakraLink asChild><NextLink href="/contact">Contact</NextLink></ChakraLink>
-                        <ChakraLink asChild>
-                            <Button>
-                                <NextLink href="tel:+16034961535">Call Us Today</NextLink>
-                            </Button>
+                    <HStack justifyContent={'space-between'} pl={16}>
+                        <ChakraLink asChild px={8}>
+                            <NextLink href="/">
+                                <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={48} />
+                            </NextLink>
                         </ChakraLink>
+                        <HStack justifyContent={'space-evenly'}>
+                            <ChakraLink asChild><NextLink href="/">
+                                Home
+                            </NextLink>
+                            </ChakraLink>
+                            <ChakraLink asChild><NextLink href="/about-us">
+                                About Us
+                            </NextLink>
+                            </ChakraLink>
+                            <ChakraLink asChild>
+                                <NextLink href="/services">
+                                    Services
+                                </NextLink>
+                            </ChakraLink>
+                            <ChakraLink asChild>
+                                <NextLink href="/contact">
+                                    Contact
+                                </NextLink>
+                            </ChakraLink>
+                            <ChakraLink asChild>
+                                <Button>
+                                    <NextLink href="tel:+16034961535">
+                                        Call Us Today
+                                    </NextLink>
+                                </Button>
+                            </ChakraLink>
+                        </HStack>
                     </HStack>
                 ) : (
                     <HStack justifyContent={'space-between'} py={3.5} px={1}>
