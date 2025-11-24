21e0ea9b9a515bfa835de61126b89ea46f33666c
Devell Robinson <devell.robinson@icloud.com>
2025-11-24 05:07:37 +0000

    feat: enhance Navbar component with text labels for improved accessibility and navigation

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { HStack, Container, Link as ChakraLink, Button, useBreakpointValue } from '@chakra-ui/react'
+import { HStack, Container, Link as ChakraLink, Button, useBreakpointValue, Text } from '@chakra-ui/react'
 
 export default function Navbar() {
 
@@ -19,38 +19,40 @@ export default function Navbar() {
         <nav>
             <Container h={'10vh'} zIndex={1} position={"absolute"} w={'100vw'} py={8}>
                 {current !== "base" && current !== "sm" ? (
-                    <HStack justifyContent={'space-between'} pl={16}>
-                        <ChakraLink asChild px={8}>
+                    <HStack justifyContent={'space-between'} px={8}>
+                        <ChakraLink asChild pl={2}>
                             <NextLink href="/">
                                 <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={48} />
                             </NextLink>
                         </ChakraLink>
-                        <HStack justifyContent={'space-evenly'}>
-                            <ChakraLink asChild><NextLink href="/">
-                                Home
-                            </NextLink>
+                        <HStack justifyContent={'space-evenly'} spaceX={6} pr={8}>
+                            <ChakraLink asChild>
+                                <NextLink href="/">
+                                    <Text textStyle={'md'} color={"white"}>Home</Text>
+                                </NextLink>
                             </ChakraLink>
-                            <ChakraLink asChild><NextLink href="/about-us">
-                                About Us
-                            </NextLink>
+                            <ChakraLink asChild>
+                                <NextLink href="/about-us">
+                                    <Text textStyle={'md'} color={"white"}>About Us</Text>
+                                </NextLink>
                             </ChakraLink>
                             <ChakraLink asChild>
                                 <NextLink href="/services">
-                                    Services
+                                    <Text textStyle={'md'} color={"white"}>Services</Text>
                                 </NextLink>
                             </ChakraLink>
                             <ChakraLink asChild>
                                 <NextLink href="/contact">
-                                    Contact
+                                    <Text textStyle={'md'} color={"white"}>Contact</Text>
                                 </NextLink>
                             </ChakraLink>
-                            <ChakraLink asChild>
-                                <Button>
+                            <Button bg={'teal.focusRing'} variant="solid" rounded="md" size={'xl'}>
+                                <ChakraLink asChild>
                                     <NextLink href="tel:+16034961535">
-                                        Call Us Today
+                                        <Text textStyle={'md'} color={"white"}>Call Us Today</Text>
                                     </NextLink>
-                                </Button>
-                            </ChakraLink>
+                                </ChakraLink>
+                            </Button>
                         </HStack>
                     </HStack>
                 ) : (
@@ -69,7 +71,7 @@ export default function Navbar() {
 
 
             </Container>
-        </nav>
+        </nav >
 
     )
 };
