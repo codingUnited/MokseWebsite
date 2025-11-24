939cbfbe1b996cfb6c87ce2b6ee8bce0f8477e63
Devell Robinson <devell.robinson@icloud.com>
2025-11-24 01:42:34 +0000

    feat: refactor FeatureCard component to use Chakra UI for improved componenet layout and styling

--- src/components/home/feature-card.tsx
+++ src/components/home/feature-card.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import type { ReactNode } from 'react';
+import { Card, Avatar, Icon } from '@chakra-ui/react';
 
 type FeatureCardProps = {
   title: string;
@@ -6,16 +9,26 @@ type FeatureCardProps = {
   icon: ReactNode;
 };
 
+
 export default function FeatureCard({
   title,
   description,
   icon,
 }: FeatureCardProps) {
+
   return (
-    <div>
-      <div>{icon}</div>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
+    <Card.Root>
+      <Card.Header>
+        <Avatar.Root my={4} boxSize={12}>
+          <Icon >{icon}</Icon>
+        </Avatar.Root>
+        <Card.Title>{title}</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <p>
+          {description}
+        </p>
+      </Card.Body>
+    </Card.Root >
   );
 }
