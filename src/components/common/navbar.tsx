07fa8ec30d4937b3589c008f404908be27ff7293
Devell Robinson <devell.robinson@icloud.com>
2025-12-02 09:19:50 +0000

    feat: update Navbar component to enhance text color handling for light mode and improve button styles

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -37,43 +37,43 @@ export default function Navbar() {
         <nav>
             <Container h={'10vh'} zIndex={1} position={isFixed ? 'fixed' : 'absolute'} fluid py={8} transition="all 0.3s ease-in-out"
                 transform={isFixed ? 'translateY(0)' : 'translateY(-20px)'}
-                opacity={isFixed ? 1 : 0.9} boxShadow={isFixed ? '2xl' : 'none'} bgColor={isFixed ? (colorMode === "light" ? 'whiteAlpha.900' : 'blackAlpha.900') : 'transparent'}>
+                opacity={isFixed ? 1 : 0.9} boxShadow={isFixed ? '2xl' : 'none'} bg={isFixed ? 'blackAlpha.950' : 'transparent'} >
                 {current !== "base" && current !== "sm" ? (
                     <HStack justifyContent={'space-between'} px={8}>
-                        <ChakraLink asChild pl={2}>
+                        <ChakraLink asChild pl={2}  >
                             <NextLink href="/">
                                 <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={48} />
                             </NextLink>
                         </ChakraLink>
-                        <HStack justifyContent={'space-evenly'} spaceX={6} pr={8}>
+                        <HStack justifyContent={'space-evenly'} spaceX={6} pr={8} >
                             <ChakraLink asChild>
                                 <NextLink href="/">
-                                    <Text textStyle={'md'} color={"white"}>Home</Text>
+                                    <Text textStyle={'md'} _light={{ color: "white" }}>Home</Text>
                                 </NextLink>
                             </ChakraLink>
                             <ChakraLink asChild>
                                 <NextLink href="/about-us">
-                                    <Text textStyle={'md'} color={"white"}>About Us</Text>
+                                    <Text textStyle={'md'} _light={{ color: "white" }}>About Us</Text>
                                 </NextLink>
                             </ChakraLink>
                             <ChakraLink asChild>
                                 <NextLink href="/services">
-                                    <Text textStyle={'md'} color={"white"}>Services</Text>
+                                    <Text textStyle={'md'} _light={{ color: "white" }}>Services</Text>
                                 </NextLink>
                             </ChakraLink>
                             <ChakraLink asChild>
                                 <NextLink href="/contact">
-                                    <Text textStyle={'md'} color={"white"}>Contact</Text>
+                                    <Text textStyle={'md'} _light={{ color: "white" }} >Contact</Text>
                                 </NextLink>
                             </ChakraLink>
                             <Button bg={'teal.focusRing'} variant="solid" rounded="md" size={'xl'}>
                                 <ChakraLink asChild>
                                     <NextLink href="tel:+16034961535">
-                                        <Text textStyle={'md'} color={"white"}>Call Us Today</Text>
+                                        <Text textStyle={'md'} _light={{ color: "white" }}>Call Us Today</Text>
                                     </NextLink>
                                 </ChakraLink>
                             </Button>
-                            <Button bg={isFixed ? 'blackAlpha.100' : 'blackAlpha.100'} variant="solid" rounded="md" size={'xl'} onClick={toggleColorMode}>
+                            <Button bg={isFixed ? (colorMode === "light" ? 'teal.focusRing' : 'teal.focusRing') : 'transparent'} variant="solid" rounded="md" size={'xl'} onClick={toggleColorMode} >
                                 {colorMode === "light" ? <MdSun /> : <MdMoon />}
                             </Button>
                         </HStack>
