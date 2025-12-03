ac2952b1a6733f1d0a7e13c4d528c8969e8548d7
Devell Robinson <devell.robinson@icloud.com>
2025-12-03 22:14:02 +0000

    feat: refactor font imports to use OpenSans and remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/common/footer";
 import { Provider } from "@/components/ui/provider";
 import Navbar from "@/components/common/navbar";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const openSans = Open_Sans({
+  weight: ["400", "600", "700"],
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
-const poppins = Poppins({
-  weight: ["400"],
-  variable: "--font-poppins",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Mokse - Empowering Change Through Education and Advocacy",
   description: "We strive to break down barriers and stop the stigma associated with incarceration through consulting services, educational conferences, and business support programs.",
@@ -32,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={`antialiased`}>
+    <html lang="en" suppressHydrationWarning>
       <body
-        
+        className={`${openSans.variable} antialiased`}
       >
         <Provider>
           <Navbar />
