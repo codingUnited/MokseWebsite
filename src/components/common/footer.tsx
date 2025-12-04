824769843b236e23b3ff4e3fc22d9c1e8f5b0848
Devell Robinson <devell.robinson@icloud.com>
2025-12-04 02:52:00 +0000

    feat: enhance Footer component with social media icons and improved layout

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -2,13 +2,17 @@
 
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { VStack, HStack, Container, Link as ChakraLink, Button, useBreakpointValue, Text, Heading, Center } from '@chakra-ui/react'
+import { VStack, HStack, Container, Link as ChakraLink, Button, useBreakpointValue, Text, Heading, Center, IconButton, Icon } from '@chakra-ui/react'
 import { useColorMode } from '../ui/color-mode';
 import { IoIosArrowForward as IoRightArrow } from "react-icons/io";
+import { MdLocationPin, MdPhone, MdOutlineMailOutline as MdMail } from "react-icons/md";
+import { FaFacebook as FB, FaLinkedin as LinkedIn, FaYoutube as YouTube, FaInstagram as Instagram } from "react-icons/fa6";
+
+import { poppins } from '@/components/ui/fonts';
 
 export default function Footer() {
 
-  const { toggleColorMode, colorMode, } = useColorMode();
+  const { colorMode } = useColorMode();
   const current = useBreakpointValue({
     base: "base",
     sm: "sm",
@@ -20,174 +24,199 @@ export default function Footer() {
 
   return (
     <footer>
-      <Container h={'50vh'} fluid pt={24}>
-        {current !== "base" && current !== "sm" ? (
-          <VStack justifyContent={'space-between'} px={8}>
-            <HStack>
-              <VStack>
-                <Image src="/mokse-logo.png" alt="Mokse" width={200} height={51} />
-                <p>A society where all individuals have equal opportunities to thrive, learn, and lead.</p>
-                <VStack alignItems={'left'}>
+      {current !== "base" && current !== "sm" ? (
 
-                  <ChakraLink asChild >
-                    <NextLink href="https://web.facebook.com/mokseorg/" target="_blank" rel="noopener noreferrer">
-                      Facebook
-                    </NextLink>
-                  </ChakraLink>
+        <Container h={'50vh'} fluid pt={24} justifyContent={'space-between'} px={8}>
 
-
-                  <ChakraLink asChild >
-                    <NextLink href="https://www.linkedin.com/company/mokse-org" target="_blank" rel="noopener noreferrer">
-                      LinkedIn
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild >
-                    <NextLink href="https://www.youtube.com/@mokse-org" target="_blank" rel="noopener noreferrer">
-                      YouTube
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild >
-                    <NextLink href="https://www.instagram.com/mokse_org/" target="_blank" rel="noopener noreferrer">
-                      Instagram
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild >
-                    <NextLink href="mailto:info@mokse.org">
-                      Email
-                    </NextLink>
-                  </ChakraLink>
-
-                </VStack>
-              </VStack>
-              <VStack>
-                <Heading as="h2">
-                  <Text textStyle={"lg"} textAlign={'center'} overflow={'hidden'} _light={{ color: "white" }}>
-                    Quick Links
-                  </Text>
-                </Heading>
-                <VStack alignItems={'left'}>
-                  <ChakraLink asChild>
-                    <NextLink href="/">
-                      <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>Home</Text>
-                      </Center>
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild>
-                    <NextLink href="/about-us">
-                      <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>About Us</Text>
-                      </Center>
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild>
-                    <NextLink href="/services">
-                      <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>Services</Text>
-                      </Center>
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild>
-                    <NextLink href="/contact">
-                      <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>Contact</Text>
-                      </Center>
-                    </NextLink>
-                  </ChakraLink>
-
-                </VStack>
-              </VStack>
-              <VStack>
-                <Heading as="h2">
-                  <Text textStyle={"lg"} textAlign={'center'} overflow={'hidden'} _light={{ color: "white" }}>
-                    Support
-                  </Text>
-                </Heading>
-                <VStack alignItems={'left'}>
-
-                  <ChakraLink asChild >
-                    <NextLink href="/terms">
-                      <Text textStyle={'md'}>Terms and Conditions</Text>
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild >
-                    <NextLink href="/about-us#legal">
-                      <Text textStyle={'md'}>Legal</Text>
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild >
-                    <NextLink href="/services">
-                      <Text textStyle={'md'}>Business</Text>
-                    </NextLink>
-                  </ChakraLink>
-
-
-                  <ChakraLink asChild >
-                    <NextLink href="/contact">
-                      <Text textStyle={'md'}>Partners</Text>
-                    </NextLink>
-                  </ChakraLink>
-
-                </VStack>
-              </VStack>
-              <VStack>
-                <Heading as="h2">
-                  <Text textStyle={"lg"} textAlign={'center'} overflow={'hidden'} _light={{ color: "white" }}>
-                    Our Company
-                  </Text>
-                </Heading>
-                <address>
-                  <p>497 Hooksett Road, Suite 362, Manchester, NH 03104</p>
-                  <p>
-                    <a href="tel:+16034961535">+1 603 496 1535</a>
-                  </p>
-                  <p>
-                    <a href="mailto:info@mokse.org">info@mokse.org</a>
-                  </p>
-                </address>
-              </VStack>
-            </HStack>
-            <HStack>
-              <p>Copyright &copy; 2025 Mokse | Powered by Mokse</p>
-            </HStack>
-
-
-            {/* <ChakraLink asChild >
-                <NextLink href="/contact">
-                  <Text textStyle={'md'} color={"white"}>Contact</Text>
+          <VStack alignItems={'start'}>
+            <Image src="/mokse-logo.png" alt="Mokse" width={200} height={51} />
+            <p>A society where all individuals have equal opportunities to thrive, learn, and lead.</p>
+            <VStack alignItems={'left'}>
+              <ChakraLink asChild >
+                <NextLink href="https://web.facebook.com/mokseorg/" target="_blank" rel="noopener noreferrer" >
+                  <IconButton variant={'outline'} border={'lg'} borderRadius={'full'} size={'lg'} color={'teal.focusRing'} p={2} aria-label="Facebook">
+                    <FB size={10} />
+                  </IconButton>
                 </NextLink>
-              </ChakraLink> */}
-
+              </ChakraLink>
+              <ChakraLink asChild >
+                <NextLink href="https://www.linkedin.com/company/mokse-org" target="_blank" rel="noopener noreferrer" >
+                  <IconButton variant={'outline'} border={'lg'} borderRadius={'full'} size={'lg'} color={'teal.focusRing'} p={2} aria-label="Facebook">
+                    <LinkedIn size={10} />
+                  </IconButton>
+                </NextLink>
+              </ChakraLink>
+              <ChakraLink asChild >
+                <NextLink href="https://www.youtube.com/@mokse-org" target="_blank" rel="noopener noreferrer" >
+                  <IconButton variant={'outline'} border={'lg'} borderRadius={'full'} size={'lg'} color={'teal.focusRing'} p={2} aria-label="Facebook">
+                    <YouTube size={10} />
+                  </IconButton>
+                </NextLink>
+              </ChakraLink>
+              <ChakraLink asChild >
+                <NextLink href="https://www.instagram.com/mokse_org/" target="_blank" rel="noopener noreferrer" >
+                  <IconButton variant={'outline'} border={'lg'} borderRadius={'full'} size={'lg'} color={'teal.focusRing'} p={2} aria-label="Facebook">
+                    <Instagram size={10} />
+                  </IconButton>
+                </NextLink>
+              </ChakraLink>
+              <ChakraLink asChild >
+                <NextLink href="mailto:info@mokse.org" target="_blank" rel="noopener noreferrer" >
+                  <IconButton variant={'outline'} border={'lg'} borderRadius={'full'} size={'lg'} color={'teal.focusRing'} p={2} aria-label="Facebook">
+                    <Icon asChild size={"md"} fill={'white'} color={'teal.focusRing'}>
+                      <MdMail />
+                    </Icon>
+                  </IconButton>
+                </NextLink>
+              </ChakraLink>
+            </VStack>
           </VStack>
-        ) : (
-          <HStack justifyContent={'space-between'} py={3.5} px={1}>
-            <ChakraLink asChild  >
-              <NextLink href="/">
-                <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={48} />
-              </NextLink>
-            </ChakraLink>
-            <Button>
-              Menu
-            </Button>
+          <VStack>
+            <Heading as="h2">
+              <Text textStyle={"lg"} textAlign={'start'} _light={{ color: "white" }} className={poppins.className} clear={'both'}>
+                Quick Links
+              </Text>
+            </Heading>
+            <VStack alignItems={'left'}>
+              <ChakraLink asChild>
+                <NextLink href="/">
+                  <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>Home</Text>
+                  </Center>
+                </NextLink>
+              </ChakraLink>
 
+
+              <ChakraLink asChild>
+                <NextLink href="/about-us">
+                  <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>About Us</Text>
+                  </Center>
+                </NextLink>
+              </ChakraLink>
+
+
+              <ChakraLink asChild>
+                <NextLink href="/services">
+                  <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>Services</Text>
+                  </Center>
+                </NextLink>
+              </ChakraLink>
+
+
+              <ChakraLink asChild>
+                <NextLink href="/contact">
+                  <Center><Text asChild color='teal' height={6} width={6} ><IoRightArrow /></Text><Text textStyle={'md'}>Contact</Text>
+                  </Center>
+                </NextLink>
+              </ChakraLink>
+
+            </VStack>
+          </VStack>
+          <VStack>
+            <Heading as="h2">
+              <Text textStyle={"lg"} textAlign={'start'} _light={{ color: "white" }} className={poppins.className} clear={'both'}>
+                Support
+              </Text>
+            </Heading>
+            <VStack alignItems={'left'}>
+
+              <ChakraLink asChild >
+                <NextLink href="/terms">
+                  <Center><Text textStyle={'md'}>
+                    Terms and Conditions
+                  </Text>
+                  </Center>
+
+
+                </NextLink>
+              </ChakraLink>
+
+
+              <ChakraLink asChild >
+                <NextLink href="/about-us#legal">
+                  <Center>
+                    <Text textStyle={'md'}>
+                      Legal
+                    </Text>
+                  </Center>
+
+                </NextLink>
+              </ChakraLink>
+
+
+              <ChakraLink asChild >
+                <NextLink href="/services">
+                  <Center>
+                    <Text textStyle={'md'}>
+                      Business
+                    </Text>
+                  </Center>
+
+                </NextLink>
+              </ChakraLink>
+
+
+              <ChakraLink asChild >
+                <NextLink href="/contact">
+                  <Center>
+                    <Text textStyle={'md'}>
+                      Partners
+                    </Text>
+                  </Center>
+                </NextLink>
+              </ChakraLink>
+
+            </VStack>
+          </VStack>
+          <VStack>
+            <Heading as="h2">
+              <Text textStyle={"lg"} textAlign={'start'} _light={{ color: "white" }} className={poppins.className} clear={'both'}>
+                Our Company
+              </Text>
+            </Heading>
+            <address>
+              <HStack> <Text asChild color='teal' height={6} width={6} ><MdLocationPin /></Text> <p>497 Hooksett Road, Suite 362, Manchester, NH 03104</p></HStack>
+              <HStack> <Text asChild color='teal' height={6} width={6} ><MdPhone /></Text>
+                <ChakraLink asChild >
+                  <NextLink href="tel:+16034961535">
+                    <Text textStyle={'md'}>
+                      +1 603 496 1535
+                    </Text>
+                  </NextLink>
+                </ChakraLink>
+              </HStack>
+              <HStack>
+                <Text asChild bg='teal' height={6} width={6} ><MdMail /></Text>
+                <ChakraLink asChild >
+                  <NextLink href="mailto:info@mokse.org">
+                    <Text textStyle={'md'}>
+                      info@mokse.org
+                    </Text>
+                  </NextLink>
+                </ChakraLink>
+              </HStack>
+
+
+            </address>
+          </VStack>
+          <HStack>
+            <p>Copyright &copy; 2025 Mokse | Powered by Mokse</p>
           </HStack>
-        )}
+        </Container >) : (
+        <HStack justifyContent={'space-between'} py={3.5} px={1}>
+          <ChakraLink asChild  >
+            <NextLink href="/">
+              <Image src="/MOKSE-3-180x46.png" alt="MOKSE Logo" width={180} height={48} />
+            </NextLink>
+          </ChakraLink>
+          <Button>
+            Menu
+          </Button>
+
+        </HStack>
+      )
+      }
 
 
-      </Container>
-
-    </footer>
+    </footer >
   );
 }
