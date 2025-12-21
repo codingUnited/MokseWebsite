b5df478332438092e0718e94e22bd489f3b5f65b
Devell Robinson <devell.robinson@icloud.com>
2025-12-21 23:03:08 +0000

    feat: refactor TeamMemberCard component to use Chakra UI for improved styling and layout

--- src/components/about/team-member-card.tsx
+++ src/components/about/team-member-card.tsx
@@ -1,17 +1,27 @@
 import Image from 'next/image';
 import type { TeamMember } from '@/data/team';
+import { Card, Text } from '@chakra-ui/react';
+import { poppins } from '../ui/fonts';
 
 export default function TeamMemberCard({ name, title, email, imagePath }: TeamMember) {
   return (
-    <article>
-      <figure>
-        <Image src={imagePath} alt={name} width={300} height={300} />
-      </figure>
-      <div>
-        <h3>{name}</h3>
-        <p>{title}</p>
-        {email && <p>{email}</p>}
-      </div>
-    </article>
+    <Card.Root width={300}  >
+      <Image src={imagePath} alt={name} width={300} height={300} />
+
+
+      <Card.Body >
+        <Card.Header as={'h3'} textAlign={'center'} className={poppins.className} p={2} textStyle={'xl'}>
+          {name}
+        </Card.Header>
+        <Card.Title textAlign={'center'} textStyle={'md'} >{title}
+          {email &&
+            <Card.Description textStyle={'md'} textAlign={'center'}>
+              {email}
+            </Card.Description>}
+        </Card.Title>
+
+      </Card.Body>
+
+    </Card.Root>
   );
 }
