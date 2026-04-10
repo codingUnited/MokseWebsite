8c57269aea3ff97d46be0e11001cb79e0aebea97
BorDevTech <bordevtech@icloud.com>
2026-04-09 22:16:57 -0400

    Refactor Programs page structure for improved readability

--- app/(Pages)/programs/page.tsx
+++ app/(Pages)/programs/page.tsx
@@ -1,12 +1,18 @@
-import { HeaderTemplate, PageBuilder } from "@/components/page-builder/template";
+import { BodyTemplate, HeaderTemplate, PageBuilder, SectionTemplate } from "@/components/page-builder/template";
 
 
 export default function Programs() {
     return (
-        <>
-            <PageBuilder>
-                <HeaderTemplate title="Programs" imageHeight="30vh" titleAlignment={2} titleLocation={100} description="Discover our innovative programs designed to empower and support justice-impacted individuals on their journey to reintegration and success. Explore our range of services, resources, and opportunities to get involved and make a positive impact in your community." />
-                Programs coming soon!
-            </PageBuilder>
-        </>)
+        <PageBuilder>
+            <HeaderTemplate
+                title={"Programs"}
+                imageHeight={"30vh"}
+                titleAlignment={2}
+                titleLocation={100}
+                description={"Discover our innovative programs designed to empower and support justice-impacted individuals on their journey to reintegration and success. Explore our range of services, resources, and opportunities to get involved and make a positive impact in your community."} />
+            <BodyTemplate>
+                <SectionTemplate>Programs coming soon!</SectionTemplate>
+            </BodyTemplate>
+        </PageBuilder>
+    )
 }
